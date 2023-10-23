64c0a5805e6eab7985907aa3e24055fbe0c2fa5f
Matheo Chavez <chavmath@gmail.com>
2023-10-22 21:19:51 -0500

    SpaceInvaders

---
+++ Space shooting/Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+    private PointManager pointManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointManager = GameObject.Find("PointManager").GetComponent<PointManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            pointManager.UpdateScore(50);
+            Destroy(gameObject);
+        }
+
+        if(collision.gameObject.tag == "Boundary")
+        {
+			Destroy(gameObject);
+		}
+
+	}
+}
